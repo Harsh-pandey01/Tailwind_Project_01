cc1794ad9f2853b74b69639ad4139628db8b1c04
Harsh Pandey <harshcse7571@gmail.com>
2025-08-24 17:35:53 +0530

    Completed the Landing Page

--- src/script.js
+++ src/script.js
@@ -1,27 +1,51 @@
-const slider = document.querySelector('#companies-container')
-const slider2 = document.querySelector('#companies-container2')
+const slider = document.querySelector("#companies-container");
+const slider2 = document.querySelector("#companies-container2");
+const slider3 = document.querySelector(".scroller");
+const faqsDownbtn = document.querySelectorAll(".ri-arrow-drop-down-line");
 
+gsap.to(
+  slider,
+  {
+    duration: 1,
+    x: -100,
+    scrollTrigger: {
+      trigger: slider, // element that triggers the animation
+      start: "top 80%", // when top of slider reaches 80% of viewport
+      end: "bottom 20%", // optional
+      scrub: true, // link animation to scroll
+    },
+  },
+  "a"
+);
 
-gsap.to(slider, {
+gsap.to(
+  slider2,
+  {
+    duration: 1,
+    x: 10,
+    scrollTrigger: {
+      trigger: slider, // element that triggers the animation
+      start: "top 80%", // when top of slider reaches 80% of viewport
+      end: "bottom 20%", // optional
+      scrub: true, // link animation to scroll
+    },
+  },
+  "a"
+);
+
+gsap.to(slider3, {
   duration: 1,
-  x: -100,
+  x: -200,
   scrollTrigger: {
-    trigger: slider,   // element that triggers the animation
-    start: "top 80%",  // when top of slider reaches 80% of viewport
-    end: "bottom 20%", // optional
-    scrub: true,       // link animation to scroll
-   
-  }
-},'a');
+    trigger: slider3,
+    start: "top 80%",
+    end: "bottom 20%",
+    scrub: true,
+  },
+});
 
-gsap.to(slider2, {
-  duration: 1,
-  x: 10,
-  scrollTrigger: {
-    trigger: slider,   // element that triggers the animation
-    start: "top 80%",  // when top of slider reaches 80% of viewport
-    end: "bottom 20%", // optional
-    scrub: true,       // link animation to scroll
-
-  }
-},'a');
+faqsDownbtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.target.parentElement.parentElement.children[1].classList.toggle("hidden");
+  });
+});
